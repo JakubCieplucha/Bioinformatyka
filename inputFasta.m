769f688fe0843f898bada24f2bacd4b805692cbb
JakubCieplucha <44356573+JakubCieplucha@users.noreply.github.com>
2019-04-25 11:48:32 +0200

    poprawa

--- inputFasta.m
+++
@@ -1,9 +0,0 @@
-%{
-This function allows the user to input the desired Fasta content
-himself.After starting you will be asked to give the identifier and the
-sequence.
-Output parameter: fastaContent- the given Fasta content.
-%}
-function fastaContent = inputFasta()
-fastaContent=['>',input('identifier: ','s'),char(10),input('sequence: ','s')];
-end
--- optimalPath.m
+++ optimalPath.m
@@ -15,14 +15,8 @@ m = length(sequence2);
 empty = '_';
 amount = length(operations);
 for i = 1 : amount
-    if n == m
-       switch operations(i)   
-            case "insertion"
-            sequence2(i) = empty;
-            case "deletion"
-            sequence1(i) = empty;
-       end
-   elseif i == 1 && operations(1) == "insertion"
+    
+   if i == 1 && operations(1) == "insertion"
          sequence1 = [empty sequence1(1:end)];
    elseif i == 1 &&  operations(1) == "deletion"
          sequence2 = [empty sequence2(1:end)];
